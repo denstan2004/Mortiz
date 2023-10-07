edc0e11a53042d55400886d795f919467eb1f6d4
Денис Станіслав <125273149+denstan2004@users.noreply.github.com>
2023-10-07 13:29:42 +0300

    azure test 2

--- Program.cs
+++ Program.cs
@@ -29,6 +29,14 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
+    // Додайте URL-переадресацію для всіх урлів на ваш базовий URL
+    app.Use((context, next) =>
+    {
+        context.Request.Scheme = "https"; // Або "http", в залежності від протоколу
+        context.Request.Host = new HostString("testappfuck.azurewebsites.net");
+        return next();
+    });
 }
 
 app.UseHttpsRedirection();
@@ -37,7 +45,6 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
 
 app.MapControllerRoute(
     name: "default",
